d5d35591402cf0626929f0dc6a8e5e0089d6aad2
joshleff <19851817-joshleff@users.noreply.replit.com>
2023-08-20 16:34:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let myhttp = require('http');
+
+let myserver = myhttp.createServer(
+  function(myrequest, myresponse) {
+    console.log(myrequest.url);
+
+    let mytext;
+      if ( myrequest.url === "/hey" ) {
+        mytext = "Well hello there folks...";
+        } else {
+        mytext = "I don't know you!";
+        }
+    
+    myresponse.writeHead(200, {"Content-Type": "text/plain"});
+    myresponse.end(mytext);
+  });
+
+myserver.listen(8080, "0.0.0.0");
+
+console.log("server has started");
